c4b6bbdf47ea34663c4b292f7ef69236ea350a3e
Thomas Stringer <thomas@trstringer.com>
2024-02-04 17:31:07 -0500

    Debug

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo "Installing ocb..."
-curl -L -o "ocb" "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
-
+curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
