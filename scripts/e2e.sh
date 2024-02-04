0ce380d5df908a895f96e1dda2aeec3d451a46d7
Thomas Stringer <thomas@trstringer.com>
2024-02-04 08:40:00 -0500

    Fix e2e

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -3,3 +3,19 @@
 echo "Running e2e tests"
 
 make deploy
+
+echo "Waiting for pods to all come up"
+until ! kubectl get po -A | grep ContainerCreating; do
+    echo "Pods still creating"
+    sleep 5
+done
+
+sleep 15
+
+if kubectl get po -A --no-headers | grep -v Running | grep -v Completed; then
+    echo "Found pods in a state other than Running or Completed"
+    exit 1
+fi
+
+echo "Cluster successfully running"
+make clean
