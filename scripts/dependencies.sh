09b82a326bda69cd604547857d1b82c9036866db
Thomas Stringer <thomas@trstringer.com>
2024-02-04 17:36:50 -0500

    Add mkdir

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 echo "Installing ocb..."
+mkdir -p ~/.local/bin
 curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
