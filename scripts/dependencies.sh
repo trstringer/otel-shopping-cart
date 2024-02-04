8ecb76f89722a1ea0567da98e032126c13d54f4d
Thomas Stringer <thomas@trstringer.com>
2024-02-04 17:42:32 -0500

    Add permissions

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Installing ocb..."
-mkdir -p /home/runner/.local/bin
-curl -L -o /home/runner/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
+mkdir -p ~/.local/bin
+curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
+chmod 755 ~/.local/bin/ocb
 echo "Running which..."
 ocb version
 # which ocb
