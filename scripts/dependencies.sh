295608b0ad13e457eeb0d28efce6d02cc828ed44
Thomas Stringer <thomas@trstringer.com>
2024-02-04 17:40:27 -0500

    Add which

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,4 +3,6 @@
 echo "Installing ocb..."
 mkdir -p /home/runner/.local/bin
 curl -L -o /home/runner/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
+echo "Running which..."
+which ocb
 # which ocb
