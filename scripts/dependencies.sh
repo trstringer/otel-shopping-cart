f439ae1024de87da3bb974c65454357921738beb
Thomas Stringer <thomas@trstringer.com>
2024-02-04 17:43:25 -0500

    Fix e2e target

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -4,6 +4,3 @@ echo "Installing ocb..."
 mkdir -p ~/.local/bin
 curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
 chmod 755 ~/.local/bin/ocb
-echo "Running which..."
-ocb version
-# which ocb
