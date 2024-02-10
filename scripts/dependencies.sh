449cc41f7cd2c22e0f1d726041c3419a129b7190
Thomas Stringer <thomas@trstringer.com>
2024-02-09 20:57:52 -0500

    Fix dep install

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,5 +2,5 @@
 
 echo "Installing ocb..."
 mkdir -p ~/.local/bin
-curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.93.0/ocb_0.93.0_linux_amd64"
+curl -L -o ~/.local/bin/ocb "https://github.com/open-telemetry/opentelemetry-collector/releases/download/cmd%2Fbuilder%2Fv0.94.1/ocb_0.94.1_linux_amd64"
 chmod 755 ~/.local/bin/ocb
