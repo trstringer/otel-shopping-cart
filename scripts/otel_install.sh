f787161ed0fc6a5fec3638e2fbc65239acea8204
Thomas Stringer <thomas@trstringer.com>
2024-03-03 15:54:42 -0500

    Create otelcol resource

--- scripts/otel_install.sh
+++ scripts/otel_install.sh
@@ -3,3 +3,7 @@
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
 sleep 30
 kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
+
+sleep 30
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+kubectl apply -f "${SCRIPT_DIR}/../collector/opentelemetry-collector.yaml"
