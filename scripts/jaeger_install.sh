f2359ee928d930cf42f19a13255f09ec9ef9b7a0
Thomas Stringer <thomas@trstringer.com>
2024-03-08 09:49:32 -0500

    Initial refactor

--- scripts/jaeger_install.sh
+++ scripts/jaeger_install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
-
 kubectl create namespace observability
 JAEGER_OPERATOR_URI="https://github.com/jaegertracing/jaeger-operator/releases/download/v1.49.0/jaeger-operator.yaml"
 until kubectl create -f "$JAEGER_OPERATOR_URI" -n observability; do
     kubectl delete -f "$JAEGER_OPERATOR_URI" -n observability
-    echo Waiting for cert-manager to be ready for jaeger
-    sleep 10
+    echo "Waiting for jaeger operator to come up..."
+    sleep 5
 done
 
 until kubectl create -f ./kubernetes/jaeger.yaml; do
-    echo Waiting for jaeger to be installed
-    sleep 10
+    echo "Waiting for jaeger to install successfully..."
+    sleep 5
 done
